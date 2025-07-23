7f1332d586737a8fb27ec547532e2333f418e48d
timmy-turner005 <timitalented005@gmail.com>
2025-07-23 17:34:39 +0100

    first commit

---
+++ src/components/accordian/Accordian.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import data from "./data";
+import "../accordian/styles.css";
+
+export default function Accordian() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultioSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  function handleMultiSelection(getCurrentId) {
+    let cpyMultiple = [...multiple];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+
+    console.log(findIndexOfCurrentId);
+    if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+
+    setMultiple(cpyMultiple);
+  }
+
+  console.log(selected, multiple);
+  return (
+    <div className="wrapper">
+      <button onClick={() => setEnableMultioSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
+
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item" key={dataItem.id}>
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="content">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="content">{dataItem.answer}</div>
+                  )}
+              {/* {selected === dataItem.id ||
+              multiple.indexOf(dataItem.id) !== -1 ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null} */}
+            </div>
+          ))
+        ) : (
+          <div>No data found</div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/accordian/data.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    id: "1",
+    question: "What are accordian components?",
+    answer:
+      "Accordion components are UI elements that display content in a vertically stacked, collapsible format",
+  },
+
+  {
+    id: "2",
+    question: "What are they used for?",
+    answer:
+      "They allow users to expand and collapse individual sections of content, saving space and making it easier to navigate long pages or lists",
+  },
+
+  {
+    id: "3",
+    question: "Accordian as a musical instrument?",
+    answer:
+      "it is a free-reed musical instrument held and played by hand, characterized by a keyboard or buttons on either side and a bellows that pumps air over reeds to produce sound",
+  },
+
+  {
+    id: "4",
+    question: "can one create an accordian with a different framework?",
+    answer:
+      "Yes, it is possible to create an accordion component using various web development frameworks and libraries",
+  },
+];
+
+export default data;
